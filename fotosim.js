eb5bc811643ceb4f6f126752e501d8f4e8582e3f
Audun Hauge <audun.hauge@gmail.com>
2016-02-05 14:32:05 +0100

    sound test added

--- fotosim.js
+++ fotosim.js
@@ -5,9 +5,10 @@
  * Creates a closure for all vars and functions
  */
 function setup() {
-	
+
   var divInterface = document.getElementById("interface");
   var divIntro = document.getElementById("intro");
+  var shutter = new Howl({ urls:[ 'shutter.mp3']});
 
   divInterface.addEventListener("click", checkClick);
   divIntro.addEventListener("click", startSimulation);
@@ -15,14 +16,15 @@ function setup() {
 
   /**
    * Check which overlay was clicked
+   * Activate intro for selected simulation
    * @param {MouseEvent} e
    */
   function checkClick(e) {
-	  var target = e.target;
-	  if (target.id === "interface") {
-		  return;
-		  // Do nothing unless an overlay is clicked
-	  }
+    var target = e.target;
+    if (target.id === "interface") {
+      return;
+      // Do nothing unless an overlay is clicked
+    }
     var idname = target.id;
     divIntro.style.visibility = "visible";
     divInterface.style.visibility = "hidden";
@@ -31,11 +33,15 @@ function setup() {
       console.log("Missing info for ", idname);
       return;
     }
+    
+    // TODO just testing sound lib
+    shutter.play();
+    
     var divTitle = document.querySelector("#lightbox h4");
     var divText = document.querySelector("#lightbox div");
     divTitle.innerHTML = info.title;
-    divText.innerHTML = info.text + '<p>Klikk for å begynne ...</p>' ;
-    divIntro.style.backgroundImage = 'url("images/sets/'+idname+'Set/index.jpg")';    
+    divText.innerHTML = info.text + '<p>Klikk for å begynne ...</p>';
+    divIntro.style.backgroundImage = 'url("images/sets/' + idname + 'Set/index.jpg")';
   }
   
   /**
@@ -46,5 +52,5 @@ function setup() {
     divIntro.style.visibility = "hidden";
     divInterface.style.visibility = "visible";
   }
-  
+
 }
