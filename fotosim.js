bc35b2096bbe9f5073d932cdf6c6ae0f7c01a4ba
Audun Hauge <audun.hauge@gmail.com>
2016-02-05 23:49:13 +0100

    overlay looking good

--- fotosim.js
+++ fotosim.js
@@ -1,3 +1,4 @@
+/* global infolist */
 "use strict";
 
 /**
@@ -8,10 +9,13 @@ function setup() {
 
   var divInterface = document.getElementById("interface");
   var divIntro = document.getElementById("intro");
+  var divSimula = document.getElementById("simula");
   var shutter = new Howl({ urls:[ 'shutter.mp3']});
+  var idname;
 
   divInterface.addEventListener("click", checkClick);
   divIntro.addEventListener("click", startSimulation);
+  divSimula.addEventListener("click", endSimulation);
 
 
   /**
@@ -25,7 +29,7 @@ function setup() {
       return;
       // Do nothing unless an overlay is clicked
     }
-    var idname = target.id;
+    idname = target.id;
     divIntro.style.visibility = "visible";
     divInterface.style.visibility = "hidden";
     var info = infolist[idname];
@@ -45,12 +49,40 @@ function setup() {
   }
   
   /**
-   * TODO for now just return to main menu
-   * 
+   * Show camera and image to focus on
+   * @param {MouseEvent} e
    */
   function startSimulation(e) {
     divIntro.style.visibility = "hidden";
+    divIntro.style.backgroundImage = 'url("images/interface.png")';
+    divSimula.style.backgroundImage = 'url("images/sets/' + idname + 'Set/index.jpg")';
+    divSimula.style.visibility = "visible";
+    document.querySelector("#controls").classList.add("controllme");
+    document.querySelector("#bigwheel").classList.add("active");
+    document.querySelector("#smallwheel").classList.add("active");
+    document.querySelector("#iso").classList.add("active");
+    document.querySelector("#fire").classList.add("active");
+    /*
+    if (divSimula.requestFullscreen) {
+      divSimula.requestFullscreen();
+    } else if (divSimula.webkitRequestFullscreen) {
+      divSimula.webkitRequestFullscreen();
+    } else if (divSimula.mozRequestFullScreen) {
+      divSimula.mozRequestFullScreen();
+    } else if (divSimula.msRequestFullscreen) {
+      divSimula.msRequestFullscreen();
+    }
+    */
+  }
+  
+  function endSimulation(e) {
+    divSimula.style.visibility = "hidden";
     divInterface.style.visibility = "visible";
+    document.querySelector("#controls").classList.remove("controllme");
+    document.querySelector("#bigwheel").classList.remove("active");
+    document.querySelector("#smallwheel").classList.remove("active");
+    document.querySelector("#iso").classList.remove("active");
+    document.querySelector("#fire").classList.remove("active");
   }
 
 }
