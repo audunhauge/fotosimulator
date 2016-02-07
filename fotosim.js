2075d2e79ab8fefb71d48f7c9aa744399749cf6d
Audun Hauge <audun.hauge@gmail.com>
2016-02-07 23:40:15 +0100

    can now snap pix

--- fotosim.js
+++ fotosim.js
@@ -1,4 +1,5 @@
 /* global infolist */
+/* global imagesets */
 "use strict";
 
 /**
@@ -7,15 +8,29 @@
  */
 function setup() {
 
-  var divInterface = document.getElementById("interface");
-  var divIntro = document.getElementById("intro");
-  var divSimula = document.getElementById("simula");
-  var shutter = new Howl({ urls:[ 'shutter.mp3']});
-  var idname;
+  /* controls */
+  var divInterface = document.getElementById("interface");  // menu
+  var divIntro = document.getElementById("intro");          // intro
+  var divSimula = document.getElementById("simula");        // simulation container
+  var divView = document.getElementById("view");            // camera preview
+  var divBack = document.getElementById("back");            // background
+  var divFire = document.getElementById("ctrl_fire");       // snap a picture
+  
+  var snapping = false;  // true while animating a snapshot
+  // The shutter sound is playing and a short animation 
+  // causes the viewfinder image to blink
+  
+  
+  
+  var shutterMP3 = new Howl({ urls:[ 'shutter.mp3']});
+  
+  var idname;       // id of image-set: nightlight, shutter ..,
+  var dataSet;      // data for this image set, attributes + imagelist
+  var attributes;   // iso, shutter, aperture settings, which controls are active
 
   divInterface.addEventListener("click", checkClick);
   divIntro.addEventListener("click", startSimulation);
-  divSimula.addEventListener("click", endSimulation);
+  divView.addEventListener("click", endSimulation);
 
 
   /**
@@ -30,6 +45,7 @@ function setup() {
       // Do nothing unless an overlay is clicked
     }
     idname = target.id;
+    dataSet = imagesets[idname];
     divIntro.style.visibility = "visible";
     divInterface.style.visibility = "hidden";
     var info = infolist[idname];
@@ -38,14 +54,14 @@ function setup() {
       return;
     }
     
-    // TODO just testing sound lib
-    shutter.play();
+    // quickref for attributes
+    attributes = dataSet.attributes;
     
     var divTitle = document.querySelector("#lightbox h4");
     var divText = document.querySelector("#lightbox div");
     divTitle.innerHTML = info.title;
     divText.innerHTML = info.text + '<p>Klikk for å begynne ...</p>';
-    divIntro.style.backgroundImage = 'url("images/sets/' + idname + 'Set/index.jpg")';
+    divIntro.style.backgroundImage = 'url(' + attributes.prependURLs + 'index.jpg)';
   }
   
   /**
@@ -53,16 +69,63 @@ function setup() {
    * @param {MouseEvent} e
    */
   function startSimulation(e) {
+    var activated;
+    var ctrl;
+    console.log(imagesets[idname]);
     divIntro.style.visibility = "hidden";
     divIntro.style.backgroundImage = 'url("images/interface.png")';
-    divSimula.style.backgroundImage = 'url("images/sets/' + idname + 'Set/index.jpg")';
+    divBack.style.backgroundImage = 'url(' + attributes.prependURLs + 'index.jpg)';
     divSimula.style.visibility = "visible";
     document.querySelector("#controls").classList.add("controllme");
-    document.querySelector("#bigwheel").classList.add("active");
-    document.querySelector("#smallwheel").classList.add("active");
-    document.querySelector("#iso").classList.add("active");
-    document.querySelector("#fire").classList.add("active");
-    /*
+    activated = "aperture,shutter,iso".split(",");
+    for (ctrl of activated) {
+      document.querySelector("#ctrl_" + ctrl).className = "passive";  
+    }
+    if (attributes.controls !== "") {
+      // if attribute.controls is not empty
+      // then it contains list of active controls
+      // otherwise all are active
+      activated = attributes.controls.split(",");
+    }
+    for (ctrl of activated) {
+      document.querySelector("#ctrl_" + ctrl).classList.remove("passive");
+      document.querySelector("#ctrl_" + ctrl).classList.add("active");  
+    }
+    document.querySelector("#ctrl_fire").classList.add("active");
+    divBack.classList.add("blur");
+    fullScreen();
+    divFire.addEventListener("click", snapshot);
+    divView.style.backgroundImage = 'url(images/sets/nightlightSet/15.jpg)';
+   
+  }
+  
+  function snapshot(e) {
+    if (snapping) return;
+    snapping = true;
+    shutterMP3.play();
+    divView.style.backgroundImage = 'url(' + attributes.prependURLs + 'index.jpg)';
+    divView.classList.add('snap');
+    setTimeout(function(e) {
+      divView.classList.remove('snap');
+      snapping = false;
+    }, 200);
+  }
+  
+  
+  function endSimulation(e) {
+    divSimula.style.visibility = "hidden";
+    divView.style.backgroundImage = 'none';
+    divBack.classList.remove("blur");
+    divInterface.style.visibility = "visible";
+    document.querySelector("#controls").classList.remove("controllme");
+    document.querySelector("#ctrl_aperture").classList.remove("active");
+    document.querySelector("#ctrl_shutter").classList.remove("active");
+    document.querySelector("#ctrl_iso").classList.remove("active");
+    document.querySelector("#ctrl_fire").classList.remove("active");
+  }
+  
+  function fullScreen() {
+     /*
     if (divSimula.requestFullscreen) {
       divSimula.requestFullscreen();
     } else if (divSimula.webkitRequestFullscreen) {
@@ -73,16 +136,6 @@ function setup() {
       divSimula.msRequestFullscreen();
     }
     */
-  }
-  
-  function endSimulation(e) {
-    divSimula.style.visibility = "hidden";
-    divInterface.style.visibility = "visible";
-    document.querySelector("#controls").classList.remove("controllme");
-    document.querySelector("#bigwheel").classList.remove("active");
-    document.querySelector("#smallwheel").classList.remove("active");
-    document.querySelector("#iso").classList.remove("active");
-    document.querySelector("#fire").classList.remove("active");
   }
 
 }
---
+++ images/sets/sets.js
@@ -0,0 +1,160 @@
+var imagesets = {
+    "daylight": 
+      {
+        "attributes": {
+          "name": "daylightLoader",
+          "prependURLs": "images/sets/daylightSet/",
+          "defaultImg": "4",
+          "ev": "15",
+          "apertureIndex": "1",
+          "shutterIndex": "14",
+          "isoIndex": "7",
+          "controls": ""
+        },
+        "ImageLoader": [
+          {   "name": "bgPhoto",  "url": "index.jpg"   },
+          {   "name": "take1",  "url": "1.jpg"   },
+          {   "name": "take2",  "url": "2.jpg"   },
+          {   "name": "take3",  "url": "3.jpg"   },
+          {   "name": "take4",  "url": "4.jpg"   },
+          {   "name": "take5",  "url": "5.jpg"   },
+          {   "name": "take6",  "url": "6.jpg"   },
+          {   "name": "take7",  "url": "7.jpg"   },
+          {   "name": "take8",  "url": "8.jpg"   },
+          {   "name": "take9",  "url": "9.jpg"   },
+          {   "name": "take10", "url": "10.jpg"  },
+          {   "name": "take11", "url": "11.jpg"  },
+          {   "name": "take12", "url": "12.jpg"  },
+          {   "name": "take13", "url": "13.jpg"  },
+          {   "name": "take14", "url": "14.jpg"  },
+          {   "name": "take15", "url": "15.jpg"  },
+          {   "name": "take16", "url": "16.jpg"  },
+          {   "name": "take17", "url": "17.jpg"  },
+          {   "name": "take18", "url": "18.jpg"  }
+        ]
+      },
+    "nightlight":
+      {
+        "attributes": {
+          "name": "nightlightLoader",
+          "prependURLs": "images/sets/nightlightSet/",
+          "defaultImg": "13",
+          "ev": "3",
+          "apertureIndex": "7",
+          "shutterIndex": "9",
+          "isoIndex": "7",
+          "controls": "shutter"
+        },
+        "ImageLoader": [
+          {  "name": "bgPhoto", "url": "index.jpg"  },
+          {   "name": "take1",  "url": "1.jpg"   },
+          {   "name": "take2",  "url": "2.jpg"   },
+          {   "name": "take3",  "url": "3.jpg"   },
+          {   "name": "take4",  "url": "4.jpg"   },
+          {   "name": "take5",  "url": "5.jpg"   },
+          {   "name": "take6",  "url": "6.jpg"   },
+          {   "name": "take7",  "url": "7.jpg"   },
+          {   "name": "take8",  "url": "8.jpg"   },
+          {   "name": "take9",  "url": "9.jpg"   },
+          {   "name": "take10", "url": "10.jpg"  },
+          {   "name": "take11", "url": "11.jpg"  },
+          {   "name": "take12", "url": "12.jpg"  },
+          {   "name": "take13", "url": "13.jpg"  },
+          {   "name": "take14", "url": "14.jpg"  },
+          {   "name": "take15", "url": "15.jpg"  },
+          {   "name": "take16", "url": "16.jpg"  },
+          {   "name": "take17", "url": "17.jpg"  },
+          {   "name": "take18", "url": "18.jpg"  },
+          {   "name": "take19", "url": "19.jpg"  }
+          
+        ]
+      },
+    "indoor":  
+      {
+        "attributes": {
+          "name": "indoorLoader",
+          "prependURLs": "images/sets/indoorSet/",
+          "defaultImg": "3",
+          "ev": "8",
+          "apertureIndex": "5",
+          "shutterIndex": "5",
+          "isoIndex": "5",
+          "controls": ""
+        },
+        "ImageLoader": [
+          {  "name": "bgPhoto", "url": "index.jpg"  },
+          {   "name": "take1",  "url": "1.jpg"   },
+          {   "name": "take2",  "url": "2.jpg"   },
+          {   "name": "take3",  "url": "3.jpg"   },
+          {   "name": "take4",  "url": "4.jpg"   },
+          {   "name": "take5",  "url": "5.jpg"   },
+          {   "name": "take6",  "url": "6.jpg"   },
+          {   "name": "take7",  "url": "7.jpg"   },
+          {   "name": "take8",  "url": "8.jpg"   },
+          {   "name": "take9",  "url": "9.jpg"   },
+          {   "name": "take10", "url": "10.jpg"  },
+          {   "name": "take11", "url": "11.jpg"  },
+          {   "name": "take12", "url": "12.jpg"  },
+          {   "name": "take13", "url": "13.jpg"  },
+          {   "name": "take14", "url": "14.jpg"  },
+          {   "name": "take15", "url": "15.jpg"  },
+          {   "name": "take16", "url": "16.jpg"  }
+        ]
+      },
+    "aperture":
+      {
+        "attributes": {
+          "name": "apertureLoader",
+          "prependURLs": "images/sets/apertureSet/",
+          "defaultImg": "5",
+          "ev": "12",
+          "apertureIndex": "4",
+          "shutterIndex": "13",
+          "isoIndex": "7",
+          "controls": "aperture"
+        },
+        "ImageLoader": [
+          {  "name": "bgPhoto", "url": "index.jpg"  },
+          {   "name": "take1",  "url": "1.jpg"   },
+          {   "name": "take2",  "url": "2.jpg"   },
+          {   "name": "take3",  "url": "3.jpg"   },
+          {   "name": "take4",  "url": "4.jpg"   },
+          {   "name": "take5",  "url": "5.jpg"   },
+          {   "name": "take6",  "url": "6.jpg"   },
+          {   "name": "take7",  "url": "7.jpg"   },
+          {   "name": "take8",  "url": "8.jpg"   },
+          {   "name": "take9",  "url": "9.jpg"   },
+          {   "name": "take10", "url": "10.jpg"  }
+        ]
+      },
+    "shutter":
+      {
+        "attributes": {
+          "name": "shutterLoader",
+          "prependURLs": "images/sets/shutterSet/",
+          "defaultImg": "8",
+          "ev": "7",
+          "apertureIndex": "2",
+          "shutterIndex": "10",
+          "isoIndex": "7",
+          "controls": "shutter"
+        },
+        "ImageLoader": [
+                    {  "name": "bgPhoto", "url": "index.jpg"  },
+          {   "name": "take1",  "url": "1.jpg"   },
+          {   "name": "take2",  "url": "2.jpg"   },
+          {   "name": "take3",  "url": "3.jpg"   },
+          {   "name": "take4",  "url": "4.jpg"   },
+          {   "name": "take5",  "url": "5.jpg"   },
+          {   "name": "take6",  "url": "6.jpg"   },
+          {   "name": "take7",  "url": "7.jpg"   },
+          {   "name": "take8",  "url": "8.jpg"   },
+          {   "name": "take9",  "url": "9.jpg"   },
+          {   "name": "take10", "url": "10.jpg"  },
+          {   "name": "take11", "url": "11.jpg"  },
+          {   "name": "take12", "url": "12.jpg"  },
+          {   "name": "take13", "url": "13.jpg"  },
+          {   "name": "take14", "url": "14.jpg"  }
+        ]
+      }
+}
