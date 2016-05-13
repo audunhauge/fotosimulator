f92e4dba9566a109ae2bc324a3ac6320e1e763c0
Audun Hauge <audun.hauge@gmail.com>
2016-05-13 09:51:41 +0200

    improved css

--- interface.js
+++ interface.js
@@ -1,31 +1,41 @@
 var infolist = {
 	daylight: {
+    link:"Manuell - dagslys",
 		title:"DAGSLYSMOTIV",
-        text:"Her kan du prøve ut hvordan blender,"
+    instruks:"Velg instillinger",
+    text:"Her kan du prøve ut hvordan blender,"
 		   + " lukker og iso-verdi spiller sammen for å gi riktig eksponering."
-           + "Kameraets programvelger står på Manuell (M)."
+           + " Kameraets programvelger står på Manuell (M)."
 	},
 	nightlight: {
+    link:"Lukkertid - nattbilde",
 		title: "NATTBILDE",
+    instruks:"Velg lukkertid",
 		text: "Her kan du prøve ut hvordan du kan bruke lang lukkertid for å få fine nattbilder."
-            + "Legg også merke til hvordan bevegelsesuskarpheten (stripene etter billysene) "
+            + " Legg også merke til hvordan bevegelsesuskarpheten (stripene etter billysene) "
 			+ "forandrer seg med lukkertiden."
 	},
 	indoor: {
+    link:"Manuell - interiør",
 		title: "INTERIØR",
+    instruks:"Velg instillinger",
 		text: "Her kan du prøve ut hvordan blender, lukker og iso-verdi spiller sammen"
 		    + " for å gi riktig eksponering." 
-            + "Kameraets programvelger står på Manuell (M)."
+            + " Kameraets programvelger står på Manuell (M)."
 	},
 	aperture: {
+    link:"Dybdeskarphet - sjakkbrett",
 		title: "DYBDESKARPHET",
+    instruks:"Velg aperture",
 		text: "Her kan du prøve ut hvordan blenderen påvirker dybdeskarpheten i bildet."
-		    + "Lukkeren stiller seg selv fordi kameraets programvelger står på AV (A hvis du bruker Nikon)." 
+		    + " Lukkeren stiller seg selv fordi kameraets programvelger står på AV (A hvis du bruker Nikon)." 
 	},
 	shutter: {
+    link:"Lukkertid - elv",
 		title: "BEVEGELSE",
+    instruks:"Velg lukkertid",
 		text: "Her kan du prøve ut hvordan lukkeren påvirker bevegelsesuskarpheten i bildet."
-		    + "Blenderen stiller seg selv fordi kameraets programvelger står på TV (S hvis du bruker Nikon)." 
+		    + " Blenderen stiller seg selv fordi kameraets programvelger står på TV (S hvis du bruker Nikon)." 
 	}
 	
 }
